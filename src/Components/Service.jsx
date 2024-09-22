0501c89c8861c8ca4bd923217a0e1bffc3f10cd8
qunnderrie1 <qunnderrie@gmail.com>
2024-09-22 11:13:58 -0400

    added photos and other changes

--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -6,7 +6,6 @@ const Service = () => {
     <section className='serviceContainer'>
 
     <div className='serviceContent container'>
-      <img src="../public/car1.jpg" width="100px" height="100px" />
     <h3 className='sectionTitle'>Our Most Featured Services</h3>
     <p className='sectionSubTitle'>Select from Our Range of Detailing Services Below</p>
     <div className='detailingCardContainer'>
